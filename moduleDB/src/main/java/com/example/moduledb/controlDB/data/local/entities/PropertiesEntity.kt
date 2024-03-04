29b4eff7a2112373c76fcaff7b378bb133f2fe0b
julian.guzman <julian.guzman@mobilityado.com>
2024-03-04 15:01:24 -0600

    [UPDATE] Updated PropertiesEntity to accept null color option

--- moduleDB/src/main/java/com/example/moduledb/controlDB/data/local/entities/PropertiesEntity.kt
+++ moduleDB/src/main/java/com/example/moduledb/controlDB/data/local/entities/PropertiesEntity.kt
@@ -4,22 +4,18 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
-import com.example.moduledb.controlDB.data.remote.response.lines.Feature
-import com.example.moduledb.controlDB.data.remote.response.lines.Geometry
-import com.example.moduledb.controlDB.data.remote.response.lines.Properties
-import com.example.moduledb.controlDB.data.remote.response.lines.Style
 import com.example.moduledb.controlDB.utils.Converters
 
 @Entity
 data class PropertiesEntity(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id", defaultValue = "0") val id: Long = 0,
     val busLineCrossing: Any?,
-    val color: String,
+    val color: String? = "",
     val desBusStop: String? = "",
     val idBusLine: String,
     val idBusSAE: String? = "",
     val idBusStop: String,
     val node: String?,
     @TypeConverters(Converters::class)
-    val brands: List<BrandsEntity>?= emptyList(),
+    val brands: List<BrandsEntity>? = emptyList(),
 )
